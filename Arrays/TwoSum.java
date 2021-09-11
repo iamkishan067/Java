afbb30472fd43d293fe37321871fef3ad725a2bd
Kishan Kandu <iamkishan067@gmail.com>
2021-09-11 12:25:15 +0530

    Update TwoSum.java

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -10,21 +10,54 @@ public class TwoSum{
         int n = arr.length;
         int t = 21;
         
+        boolean ans1 = findTwoSum1(arr, t);
+        System.out.println("Found Two Sum = " + ans1);
+        
+        int[] ans2 = findTwoSum2(arr, t);
+        System.out.println(Arrays.toString(ans2));
+        
+    }
+    
+    
+    /*
+    * this is optimized method 
+    * T.C. = O(n)
+    * S.C. = O(n)
+    */
+    public static boolean findTwoSum1(int[] arr, int t){
         ArrayList<Integer> temp = new ArrayList<>();
         boolean found = false;
         
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             int x = t - arr[i];
             if(!temp.contains(arr[i])){
                 temp.add(x);
-                System.out.println(temp);
             }else{
                 found = true;
                 break;
             }
         }
         
-        System.out.println(found);
-        
+        return found;
     }
+    
+    /*
+    * this is two pointer method 
+    * T.C. = O(n)
+    * S.C. = O(1)
+    */
+    public static int[] findTwoSum2(int[] arr, int t){
+        int s = 0;
+        int e = arr.length - 1;
+        
+        while(s < e){
+            int sum = arr[s] + arr[e];
+            if(sum == t) return new int[] {s, e};
+            if(sum < t) s++;
+            else e--;
+        }
+        
+        return new int[] {-1, -1};
+    }
+    
 }
