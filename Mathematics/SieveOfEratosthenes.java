e9293997b182d77a735875cfb78ae1440c902fa4
Kishan Kandu <iamkishan067@gmail.com>
2021-09-21 16:45:30 +0530

    Create SieveOfEratosthenes.java

---
+++ Mathematics/SieveOfEratosthenes.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class SieveOfEratosthenes{
+    public static void main(String[] args){
+        boolean[] isPrime = sieveOfErastosthenes(1000);
+        
+        int i = 0;
+        for(boolean ele: isPrime){
+            System.out.println(i + "-->" +ele);
+            i++;
+        }
+    }
+    
+    public static boolean[] sieveOfErastosthenes(int n){
+        boolean[] isPrime = new boolean[n];
+        
+        Arrays.fill(isPrime, true);
+        isPrime[0] = false;
+        isPrime[1] = true;
+        
+        for(int i = 2; i * i <= n; i++){
+            for(int j = i * 2; j < n; j += i){
+                isPrime[j] = false;
+            }
+        }
+        
+        return isPrime;
+    }
+    
+}
